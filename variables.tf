861d953be1615f917efbaa0016b007c1b5489eae
guruvamsi1999 <72000709+guruvamsi1999@users.noreply.github.com>
2024-06-07 10:18:39 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,18 @@
+variable "region" {
+  description = "AWS Region"
+  default     = "us-west-2"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  default     = "10.0.0.0/16"
+}
+
+variable "subnet_cidr" {
+  description = "CIDR block for the subnet"
+  default     = "10.0.1.0/24"
+}
+
+variable "container_image" {
+  description = "Docker image URL for the app"
+}
